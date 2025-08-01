820b1f824ef92781ee6adf349e9625ccd0b35402
Kishor-g-25 <kishorg.gangannan@gmail.com>
2025-08-02 00:06:21 +0530

    Created a java file to calculate the lenght of the string

---
+++ findLength.java
@@ -0,0 +1,10 @@
+public class findLength
+{
+    public static void main(String[] args)
+    {
+        String name="Hello";
+        //int length = name.length();
+        System.out.println(name.length());
+
+    }
+}
